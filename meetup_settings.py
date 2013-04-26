3b8e0029087bf6853129f83e063c3691aa759ece
Thomas Levine <occurrence@thomaslevine.com>
2013-04-26 13:36:13 -0400

    glue

---
+++ meetup_settings.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+import os
+from lxml.html import fromstring
+from requests import session
+from time import sleep
+from random import normalvariate
+s = session()
+
+def login():
+    s.post('https://secure.meetup.com/login/', {
+        'email': os.environ['MEETUP_EMAIL_ADDRESS'],
+        'password': os.environ['MEETUP_PASSWORD'],
+        'rememberme': 'on',
+        'submitButton': 'Log in',
+        'returnUri': '',
+        'op': 'login',
+        'apiAppName': '',
+    })
+    return s
+
+def unsubscribe(s, meetup_url):
+    r = s.get(meetup_url)
+    html = fromstring(r.text)
+    board_id = html.cssselect('.D_form input')[-2].xpath('@name')[0].split('_')[1]
+    s.post(meetup_url, {
+        'eventUpd': '1',
+        'evRemind': '1',
+        'ePhotCom': '1',
+        'faceTag': '1',
+        'photoCom': '1',
+        'memFrnd': '1',
+        ('board_' + board_id): board_id,
+        'submit': 'submit',
+        'submit': 'Submit',
+    })
+    return s
+
+def main():
+    s = login()
+    r = s.get('http://www.meetup.com/account/comm/')
+    html = fromstring(r.text)
+    meetups = html.xpath('//a[text()="Email and notification settings"]/@href')
+    for meetup in meetups:
+        print meetup
+        s = unsubscribe(s, meetup)
+        sleep(normalvariate(10, 5))
+
+if __name__ == '__main__':
+    main()
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 Change Meetup email settings
 ====
+This disables the emails that Tom finds annoying for all of your meetups.
+If you want emails for some meetups, you could run this and then change those
+later. Or you can extend this to handle the exceptions. Run it like so.
 
     export MEETUP_EMAIL_ADDRESS=abc@def.ghi
     export MEETUP_PASSWORD=aBcd3fGhi
